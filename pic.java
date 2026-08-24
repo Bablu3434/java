d79bf406160e6dd1c21d65c9b3e9c617b84fb55f
Bablu Raj <babluraj32005@gmail.com>
2026-08-24 21:09:53 +0530

    java/pic

---
+++ pic.java
@@ -0,0 +1,21 @@
+public class pic {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("              *****");
+        System.out.println("            *********");
+        System.out.println("           **  ***  **");
+        System.out.println("           *  (o o)  *");
+        System.out.println("           *    ^    *");
+        System.out.println("           *  \\___/  *");
+        System.out.println("            \\_______/");
+        System.out.println("               |||");
+        System.out.println("          =====|||=====");
+        System.out.println("               |||");
+        System.out.println("            ___|||___");
+        System.out.println("           /    |    \\");
+        System.out.println("          /_____|_____\\");
+        System.out.println();
+        System.out.println("        JAI SHREE KRISHNA");
+    }
+}
