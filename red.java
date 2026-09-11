7adedca84df605944796946c201328a76066a64c
Bablu Raj <babluraj32005@gmail.com>
2026-09-11 07:04:27 +0530

    java/red

---
+++ red.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ public class red{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int button= sc.nextInt();
+        if (button ==1){
+            System.out.println("hello");
+        }
+        else if (button ==2){
+            System.out.println("namste");
+        }
+        else if (button ==3){
+            System.out.println("bonjor");
+        }
+        else{
+            System.out.println("invalid button");
+        }
+        sc.close();
+    }
+ }
