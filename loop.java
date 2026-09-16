5d15d5bab71e3e7e1792be77e50f0fba7d35c35a
Bablu Raj <babluraj32005@gmail.com>
2026-09-16 07:14:03 +0530

    java/tab

--- loop.java
+++ loop.java
@@ -1,24 +1,39 @@
-// public class loop {
-//     public static void main(String args[]){
-//         for(int i=0;i<90;i++){
-//             System.out.println("Hello World");
-//         }
-//     }
+public class loop {
+    public static void main(String args[]){
+        String i  = "RAJ GUPTA";
+        for (int j = 1 ; j <10 ; j++){
+            System.out.println(i);
+            i = i + "RAJ GUPTA";
+        }
+        
+    }
+}
     
-// }
+
 
 //prints to the number of 1 to 1oo in java program 
 
-public class loop{
-    public static void main (String args[]){
-    //     for (int  i = 1; i <10 ; i++){
-    //         System.out.println(i+" ");
-    //     }
+// public class loop{
+//     public static void main (String args[]){
+//     //     for (int  i = 1; i <10 ; i++){
+//     //         System.out.println(i+" ");
+//     //     }
 
-    int i = 1 ;
-    while (i <100){
-        System.out.println(i);
-        i = i+1;
-    }
-    }
-}
+// //     int i = 1 ;
+// //     while (i <100){
+// //         System.out.println(i);
+// //         i = i+1;
+// //         }
+// //     }
+// }
+
+// public class loop {
+//     public static void main(String args[]){
+//         int i = 1;
+//         do {
+//             System.out.println(i);
+//             i = i+1;
+//         }
+//          while (i < 100);
+//     }
+// }
