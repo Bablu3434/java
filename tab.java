9801d292308303115effbd261644685974980635
Bablu Raj <babluraj32005@gmail.com>
2026-09-17 06:37:24 +0530

    java/star

--- tab.java
+++ tab.java
@@ -9,3 +9,29 @@ public class tab {
         sc.close();
     }
 }
+
+
+// import java.util.*;
+// public class tab{
+//     public  static void main(String args[]){
+//         Scanner sc = new Scanner (System.in);
+//         for (int i = 1 ; i < 101 ; i++){
+//             System.out.println(i);
+//         }
+//         sc.close();
+//     }
+// }
+
+// import java.util.*;
+// public class tab{
+//     public static void main (String ags[]){
+//         Scanner sc = new Scanner (System.in);
+//         int n = sc.nextInt();
+//         int sum = 0;
+//         for (int i = 1 ; i<=n ; i++){
+//             sum = sum + i ;
+//         }
+//         System.out.println(sum);
+//         sc.close();
+//     }
+// }
