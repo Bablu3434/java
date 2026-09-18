74e36548b138cf6688781bc27edc36cc35f1231e
Bablu Raj <babluraj32005@gmail.com>
2026-09-18 06:29:09 +0530

    java/numbers

--- star.java
+++ star.java
@@ -1,7 +1,70 @@
+// rectangle pattern in java
 public class star {
     public static void main(String args[]){
         for( int i = 1 ; i <= 4 ; i++){
-            System.out.println("*");
+            for (int j = 1 ; j <= 5 ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
+
+public class star {
+    public static void main(String args[]){
+    int n = 4;
+    int m = 5;
+    
+    for (int i =1; i <= n ; i++){
+        for (int j = 1 ; j <= m ; j++){
+            if (i == 1 || j ==1 || i ==n || j == m ){
+            System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();   
+    }
+  }
+}
+
+public class star{
+    public static void main (String args[]){
+      int n = 4;
+      for (int i = 1 ; i <= n ; i++){
+        for (int j =1 ; j <= i ; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+      }  
+    }
+}
+
+public class star{
+    public static void main(String args[]){
+        int n = 4 ;
+        for (int i= n ; i >= 1 ; i--){
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+public class star{
+    public static void main (String args[]){
+        int n = 4 ;
+        for (int i = 1 ; i <= n ; i++){
+            for (int j = 1 ; j <= n-i ; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
